285c078f52a6495841b9f626f17f2486e9a4bbd2
Rabbi Ahmed Limon <rabbiahemed>
2023-01-26 23:48:56 +0600

    blog card

--- src/Components/BlogCard/BlogCard.js
+++ src/Components/BlogCard/BlogCard.js
@@ -1,23 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Button, Card } from "react-bootstrap";
 
-const BlogCard = () => {
-  const [blog, setBlog] = useState([]);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+const BlogCard = ({ blog }) => {
+  const { userId, title, body } = blog;
   return (
     <Card style={{ width: "100%" }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{body}</Card.Text>
+        <Button variant="primary">Read</Button>
       </Card.Body>
     </Card>
   );
--- src/Components/BlogSection/BlogSection.js
+++ src/Components/BlogSection/BlogSection.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BlogCard from "../BlogCard/BlogCard";
-
+import "./BlogSection.css";
 const BlogSection = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
   return (
-    <div>
-      <BlogCard></BlogCard>
+    <div className="blogs-container">
+      {blogs.map((blog) => (
+        <BlogCard blog={blog} key={blog.id}></BlogCard>
+      ))}
     </div>
   );
 };
